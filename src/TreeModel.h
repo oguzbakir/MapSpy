3e3eb54da18afa32e34e0d367086eff85535df8d
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2018-08-08 18:16:44 +0300

    Added tree classes

---
+++ src/TreeModel.h
@@ -0,0 +1,40 @@
+//
+// Created by oguz on 08.08.2018.
+//
+
+#ifndef MAPSPY_TREEMODEL_H
+#define MAPSPY_TREEMODEL_H
+
+#include <QAbstractItemModel>
+#include <QModelIndex>
+#include <QVariant>
+
+class TreeItem;
+
+class TreeModel : public QAbstractItemModel {
+
+    Q_OBJECT
+
+public:
+    explicit TreeModel(const QString &data, QObject *parent = 0);
+    ~TreeModel();
+
+    QVariant data(const QModelIndex &index, int role) const override;
+    Qt::ItemFlags flags(const QModelIndex &index) const override;
+    QVariant headerData(int section, Qt::Orientation orientation,
+                        int role = Qt::DisplayRole) const override;
+    QModelIndex index(int row, int column,
+                      const QModelIndex &parent = QModelIndex()) const override;
+    QModelIndex parent(const QModelIndex &index) const override;
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+
+private:
+    void setupModelData(const QStringList &lines, TreeItem *parent);
+
+    TreeItem *rootItem;
+
+};
+
+
+#endif //MAPSPY_TREEMODEL_H
